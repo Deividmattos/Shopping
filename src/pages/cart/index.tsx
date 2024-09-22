37a379f174ab7be8a8828edd104aa4b93021fa92
devDeivid <deividmattos97@gmail.com>
2024-09-22 20:08:12 +0100

    Create Context

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,34 +1,54 @@
+import { useContext } from 'react'
+import { Link  } from 'react-router-dom'
+import { CartContext } from '../../contexts/CartContext'
+
+
 export function Cart(){
+   const { cart, total, addItemCart, removeItemCart } = useContext(CartContext);
+
       return(
             <div className="w-full max-w-7x1 px-4 mx-auto">
                   <h1 className="font-medium text-2x1 text-center my-6">My Shopping</h1>
-            <section className="flex items-center justify-between border-b-2">
-            <img
-              className="w-28 rounded-lg max-h-70 mb-2"
-              src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
-              alt="Logo do produto"
-           />
-           <strong>
-              R$ 1000
-            </strong>
-            <div className="flex item-center justify-center gap-3">
-                  <button className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
-                        -
-                  </button>
-                  1
-                  <button className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
-                        +
-                  </button>
+           
+           {cart.length === 0 && (
+            <div className='flex flex-col items-center justify-center'>
+                  <p className='font-medium'>Ops seu carrinho esta vazio...</p>
+                  <Link to="/" className="bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded">
+                  Acessar Produtos
+                  </Link>
             </div>
+           )}
+           
+            {cart.map( (item) => (
+                  <section key={item.id} className="flex items-center justify-between border-b-2">
+                  <img
+                    src={item.cover}
+                    alt={item.title}
+                    className="w-28"
+                 />
+                 <strong>Price: {item.price}</strong>
 
-            <strong className="float-right">
-                  SubTotal:R$ 1000
-            </strong>
-            </section>
+                  <div className="flex item-center justify-center gap-3">
+                        <button  onClick={ () => removeItemCart(item)} className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
+                              -
+                        </button>
+                        {item.amount}
+                        <button onClick={ () => addItemCart(item)} className="bg-slate-600  px-2 rounded text-white font-medium flex items-center justify-center">
+                              +
+                        </button>
+                  </div>
+      
+                  <strong className="float-right">
+                        SubTotal:{item.total.toLocaleString("pt-BR",{
+                               style:"currency",
+                               currency:"BRL"  
+                        })}
+                  </strong>
+                  </section>
+            ) )}
+            
 
-            <p className="font-bold mt-4">
-                  Total: R$1.000
-            </p>
+          {cart.length !== 0 &&   <p className="font-bold mt-4">Total: {total}</p>}
             </div>
       )
 }
